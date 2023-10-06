e87e3ef8e8c7e38575865d09293cb5e372f0a5cc
nullpointer-excelsior <red.tiger.developer@proton.me>
2023-10-06 16:48:41 -0300

    unused apps were removed

--- apps/store-ms/src/store-ms.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StoreMsController } from './store-ms.controller';
-import { SharedModule } from '../../../libs/shared/src';
-
-@Module({
-  imports: [SharedModule,],
-  controllers: [StoreMsController],
-})
-export class StoreMsModule {}
