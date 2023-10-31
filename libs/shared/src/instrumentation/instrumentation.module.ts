9b1bde0d5f5e31a6d55184640ba3aee3db1b29c9
nullpointer-excelsior <red.tiger.developer@proton.me>
2023-10-30 23:55:23 -0300

    instrumentation module refactored

--- libs/shared/src/instrumentation/instrumentation.module.ts
+++ libs/shared/src/instrumentation/instrumentation.module.ts
@@ -1,80 +1,19 @@
-import { Module, OnApplicationBootstrap } from '@nestjs/common';
-import { HttpModule, HttpService } from '@nestjs/axios';
+import { Module } from '@nestjs/common';
 import { OpenTelemetryModule } from 'nestjs-otel';
-import { instrumentationConfigRepository } from './opentelemetry/instrumentation-config.repository';
-import { getTracer } from './opentelemetry/get-tracer';
-import { InternalAxiosRequestConfigWithSpan, injectFromResponse, setPropagation } from './axios/http.interceptors';
 import { CustomMetricsService } from './services/custom-metrics.service';
-import { createSdk } from './opentelemetry/start-opentelemetry';
-
-interface InstrumentationOptions {
-    serviceName: string
-    serviceVersion: string,
-    traceExporterOptions?: { url: string, headers?: any },
-    metricExporterOptions?: { url: string, headers?: any },
-}
 
 const opentelemetryModule = OpenTelemetryModule.forRoot() 
 
-const httpModule = HttpModule.register({})
-
 @Module({
-    imports: [opentelemetryModule, httpModule],
-    exports: [opentelemetryModule, httpModule, CustomMetricsService],
+    exports: [
+        opentelemetryModule,
+        CustomMetricsService
+    ],
     providers: [
         CustomMetricsService
     ]
 })
-export class InstrumentationModule implements OnApplicationBootstrap {
+export class InstrumentationModule {
 
-    constructor(private readonly http: HttpService) {}
-    
-    onApplicationBootstrap() {
-
-        const setPropagationInterceptor = (config: InternalAxiosRequestConfigWithSpan) => {
-            const tracer = InstrumentationModule.getTracer()
-            return setPropagation(tracer, config)
-        }
-    
-        this.http.axiosRef.interceptors.request.use(setPropagationInterceptor)
-        this.http.axiosRef.interceptors.response.use(injectFromResponse)
-
-    }
-
-    static createSdk(options: InstrumentationOptions) {
-        console.log(`Instrumentation for ${options.serviceName} module starting...`)
-        const sdk = createSdk({ 
-            ...options
-        })
-
-        instrumentationConfigRepository.save({
-            serviceName: options.serviceName, 
-            serviceVersion: options.serviceVersion 
-        })
-
-        process.on('SIGTERM', () => {
-            sdk
-                .shutdown()
-                .then(() => console.log('Tracing terminated'))
-                .catch((error) => console.log('Error terminating tracing', error))
-                .finally(() => process.exit());
-        });
-    
-        process.on('SIGINT', () => {
-            sdk
-                .shutdown()
-                .then(() => console.log('Tracing terminated'))
-                .catch((error) => console.log('Error terminating tracing', error))
-                .finally(() => process.exit());
-        });
-
-        return sdk
-
-    }
-
-    static getTracer() {
-        const resource = instrumentationConfigRepository.getResourceConfig()
-        return getTracer(resource.service, resource.version)
-    }
 }
 
--- libs/shared/src/instrumentation/opentelemetry/get-tracer.ts
+++
@@ -1,5 +0,0 @@
-import opentelemetry from "@opentelemetry/api"
-
-export function getTracer(name: string, version: string) {
-    return opentelemetry.trace.getTracer(name, version)
-}
