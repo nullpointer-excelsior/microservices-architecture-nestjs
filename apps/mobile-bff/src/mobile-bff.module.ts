9bcfc06f7f461c22bee0d9026fecbe69956d5b2b
nullpointer-excelsior <red.tiger.developer@proton.me>
2023-11-05 21:05:37 -0300

    mobile-bff module updated

--- apps/mobile-bff/src/mobile-bff.module.ts
+++ apps/mobile-bff/src/mobile-bff.module.ts
@@ -4,17 +4,17 @@ import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { AlbumResolver } from './music-discovery/graphql/resolvers/album.resolver';
-import { ArtistResolver } from './music-discovery/graphql/resolvers/artist.resolver';
-import { GenreResolver } from './music-discovery/graphql/resolvers/genre.resolver';
-import { AlbumService } from './music-discovery/music-library-api/services/album.service';
-import { ArtistService } from './music-discovery/music-library-api/services/artist.service';
-import { GenreService } from './music-discovery/music-library-api/services/genre.service';
-import { MusicLibraryCLient } from './music-discovery/shared/client/music-library.client';
-import { SongService } from './music-discovery/music-library-api/services/song.service';
-import { RadioResolver } from './music-discovery/graphql/resolvers/radio.resolver';
-import { RadioService } from './music-discovery/music-library-api/services/radio.service';
-import { PlayerCLient } from './music-discovery/shared/client/player.client';
+import { AlbumResolver } from './music-catalog/graphql/resolvers/album.resolver';
+import { ArtistResolver } from './music-catalog/graphql/resolvers/artist.resolver';
+import { GenreResolver } from './music-catalog/graphql/resolvers/genre.resolver';
+import { AlbumService } from './music-catalog/music-library-api/services/album.service';
+import { ArtistService } from './music-catalog/music-library-api/services/artist.service';
+import { GenreService } from './music-catalog/music-library-api/services/genre.service';
+import { MusicLibraryCLient } from './music-catalog/shared/client/music-library.client';
+import { SongService } from './music-catalog/music-library-api/services/song.service';
+import { RadioResolver } from './music-catalog/graphql/resolvers/radio.resolver';
+import { RadioService } from './music-catalog/music-library-api/services/radio.service';
+import { PlayerCLient } from './music-catalog/shared/client/player.client';
 import { OpenTelemetryModule } from 'nestjs-otel';
 
 @Module({
