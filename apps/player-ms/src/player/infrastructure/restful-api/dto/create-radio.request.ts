e929cce150b68d23e866fec907637bf2c75824c4
nullpointer-excelsior <red.tiger.developer@proton.me>
2023-11-23 00:11:02 -0300

    player-ms removed

--- apps/player-ms/src/player/infrastructure/restful-api/dto/create-radio.request.ts
+++
@@ -1,4 +0,0 @@
-import { OmitType } from "@nestjs/swagger";
-import { RadioModel } from "../../../domain/model/radio.model";
-
-export class CreateRadioRequest extends OmitType(RadioModel, ['id'] as const) {}
--- apps/player-ms/src/shared/events/album-created.event.ts
+++
@@ -1,7 +0,0 @@
-import { Album } from "../database/entities/album.entity";
-
-export class AlbumCreatedEvent {
-
-    constructor(public album: Album) {}
-    
-}
