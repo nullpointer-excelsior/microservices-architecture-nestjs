0df89b040c3d473a96f24c3cbaf55713d7418b32
nullpointer-excelsior <red.tiger.developer@proton.me>
2023-11-29 11:36:24 -0300

    update-songs-by-radio dto class created to refactor radio-use-cases

---
+++ apps/music-discovery-ms/src/radio/application/dto/update-songs-by-radio.dto.ts
@@ -0,0 +1,3 @@
+import { UpdateSongsDTO } from "./update-songs.dto";
+
+export type UpdateSOngsByRadioDTO = { radioId: string } & UpdateSongsDTO
--- apps/music-discovery-ms/src/radio/application/radio.use-cases.ts
+++ apps/music-discovery-ms/src/radio/application/radio.use-cases.ts
@@ -1,10 +1,10 @@
-import { Injectable } from "@nestjs/common";
 import { NotFoundExceptionIfUndefined } from "@lib/utils/decorators/not-found-exeption-if-undefined";
+import { Injectable } from "@nestjs/common";
 import { Radio } from "../domain/model/radio.model";
 import { RadioRepository } from "../domain/repositories/radio.repository";
-import { CreateRadioDTO } from "./dto/create-radio.dto";
-import { UpdateSongsDTO } from "./dto/update-songs.dto";
 import { ValidateArgumentModel } from "../infrastructure/validate-model";
+import { CreateRadioDTO } from "./dto/create-radio.dto";
+import { UpdateSOngsByRadioDTO as UpdateSongsByRadioDTO } from "./dto/update-songs-by-radio.dto";
 
 @Injectable()
 export class RadioUseCases {
@@ -19,7 +19,7 @@ export class RadioUseCases {
         return this.repository.findById(id)
     }
 
-    async updateSongs(dto: { radioId: string } & UpdateSongsDTO) {
+    async updateSongs(dto: UpdateSongsByRadioDTO) {
         return this.findRadioById(dto.radioId)
             .then(radio => Radio.updateSongs(radio, dto.songs))
             .then(radio => this.updateRadio(radio))
