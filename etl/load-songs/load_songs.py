3c5f9530fd3705a2376f044acbca59c2cb000ee5
nullpointer-excelsior <red.tiger.developer@proton.me>
2023-12-18 16:45:07 -0300

    etl load_songs now is loading mp3 files into minio storage

--- etl/load-songs/load_songs.py
+++ etl/load-songs/load_songs.py
@@ -6,12 +6,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MINIO_HOSTNAME = f"{os.environ.get('MINIO_HOSTNAME')}:{os.environ.get('MINIO_PORT')}"
+MINIO_HOSTNAME = f"{os.environ.get('MINIO_HOST')}:{os.environ.get('MINIO_PORT')}"
 ACCESS_KEY_ID = os.environ.get('MINIO_ACCESS_KEY')
 SECRET_ACCESS_KEY = os.environ.get('MINIO_SECRET_KEY')
 MUSIC_LIBRARY_DIRECTORY = "library"
 BUCKET_NAME = os.environ.get('MUSIC_LIBRARY_BUCKET_NAME')
 MUSIC_LIBRARY_MS = 'http://localhost:3011'
+
+print(MINIO_HOSTNAME, ACCESS_KEY_ID, SECRET_ACCESS_KEY)
 
 def create_genre(genre):
     res = requests.post(f"{MUSIC_LIBRARY_MS}/genres", data={'name': genre}) 
@@ -83,19 +85,18 @@ def build_library(songs):
         library[genre][artist][album].append(song)
     return library
     
-library = build_library(songs)
 
-for genre_name, genres in library.items():
-    saved_genre = create_genre(genre_name)
-    for artist_name, artists in genres.items():
-        saved_artist = create_artist(artist_name)
-        for album_name, album in artists.items():
-            saved_album = create_album(saved_artist, album_name)
-            for song in album:
-                res = create_song(saved_genre, saved_artist, saved_album, song)
-                print(res)
+def save_library(library):
+    for genre_name, genres in library.items():
+        saved_genre = create_genre(genre_name)
+        for artist_name, artists in genres.items():
+            saved_artist = create_artist(artist_name)
+            for album_name, album in artists.items():
+                saved_album = create_album(saved_artist, album_name)
+                for song in album:
+                    res = create_song(saved_genre, saved_artist, saved_album, song)
+                    print(f"saved {res['title']}")
     
-   
 
 def main():
     # init client
@@ -106,20 +107,23 @@ def main():
         secret_key=SECRET_ACCESS_KEY
     )
     # verify bucket
-    if client.bucket_exists(BUCKET_NAME):
+    if not client.bucket_exists(BUCKET_NAME):
         client.make_bucket(BUCKET_NAME)
+    else:
+        print(f"{BUCKET_NAME} already exists")
 
-    # for song in get_song_info(MUSIC_LIBRARY_DIRECTORY):
-    #     res = client.fput_object(
-    #         "music-library", f"{song['genre']}/{song['artist']}/{song['album']}/{song['song']}", song['filepath'],
-    #     )
-    #     print(res)
+    resource = get_music_library_resources(MUSIC_LIBRARY_DIRECTORY)
+    songs = [resource for resource in resource if resource['type'] == 'audio']
+    library = build_library(songs)
+    save_library(library)
+    for resource in resources:
+        client.fput_object(BUCKET_NAME, resource['object'], resource['filepath'])
 
 
 
 
-# if __name__ == "__main__":
-#     try:
-#         main()
-#     except S3Error as exc:
-#         print("error occurred.", exc)
+if __name__ == "__main__":
+    try:
+        main()
+    except S3Error as exc:
+        print("error occurred.", exc)
