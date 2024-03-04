1ff8c741a86f7d2c9a4d72d630a4a63d2aad4146
nullpointer-excelsior <red.tiger.developer@proton.me>
2024-03-04 17:08:32 -0300

    Update event-emitter.eventbus and index.ts

--- libs/utils/src/seedwork/index.ts
+++ libs/utils/src/seedwork/index.ts
@@ -3,3 +3,4 @@ export * from './domain/events/domain.event';
 export * from './domain/events/eventbus';
 export * from './infrastructure/domain-eventbus/domain-eventbus.module';
 export * from './infrastructure/domain-eventbus/services/event-emitter.eventbus';
+export * from './infrastructure/utils/generate-uuid';
--- libs/utils/src/seedwork/infrastructure/domain-eventbus/services/event-emitter.eventbus.ts
+++ libs/utils/src/seedwork/infrastructure/domain-eventbus/services/event-emitter.eventbus.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { DomainEventBus } from "../../../domain/events/eventbus";
 import { DomainEvent } from "../../../domain/events/domain.event";
 import { EventEmitter2 } from "@nestjs/event-emitter";
@@ -6,9 +6,12 @@ import { EventEmitter2 } from "@nestjs/event-emitter";
 @Injectable()
 export class EventEmitterEventbus implements DomainEventBus {
 
+    private readonly logger = new Logger(EventEmitterEventbus.name);
+
     constructor(private eventEmitter: EventEmitter2) {}
    
     async publish<T extends DomainEvent<any>>(event: T) {
+        this.logger.debug(`Publishing event: ${event.name}`);
         this.eventEmitter.emit(event.name, event);
     }
 
